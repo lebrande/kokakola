31fc1d4fa1b18613c620ff91122a91bffb7006e6
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-09-12 21:23:43 +0200

    Install rxjs.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import { fromEvent } from 'rxjs';
+
+const buttons = document.querySelectorAll('.coin');
+
+const coin$ = fromEvent(buttons, 'click');
+
+coin$.subscribe((e) => console.log(e));
