f695fd1280f4458d367f814aa5bf279ed1458cd2
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-09-12 21:55:47 +0200

    Scan coins, get credit sum.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import { fromEvent } from 'rxjs';
+import { map, scan, startWith } from 'rxjs/operators';
 
 const buttons = document.querySelectorAll('.coin');
+const creditDisplay = document.getElementById('credit');
 
-const coin$ = fromEvent(buttons, 'click');
+const coin$ = fromEvent(buttons, 'click').pipe(
+  map((e) => +e.currentTarget.innerText),
+);
 
-coin$.subscribe((e) => console.log(e));
+const credit$ = coin$.pipe(
+  scan((credit, coin) => credit + coin, 0),
+  startWith(0),
+);
+
+credit$.subscribe((credit) => {
+  creditDisplay.innerText = credit;
+});
